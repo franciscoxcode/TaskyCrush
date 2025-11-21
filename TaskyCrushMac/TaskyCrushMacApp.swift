740f9fc5e893686f99c4e9c8ef7fd9a3975efe26
Francisco Casillas <chiccasillas@gmail.com>
2025-11-20 18:23:54 -0600

    feat: macos-target: add initial macos target

---
+++ TaskyCrushMac/TaskyCrushMacApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct TaskyCrushMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, TaskyCrush for macOS!")
+                .frame(minWidth: 320, minHeight: 200)
+        }
+    }
+}
